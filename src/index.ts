05c53b9be3df8ba7471a3f657dae1338b99e04f7
Alex Anderson <alexanderson1993@gmail.com>
2020-11-05 10:54:28 -0500

    Added the win condition checking, along with locking the game when a win condition is met.

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ let boardState: TicTacToeBoard = [
 ];
 
 let currentMove: "X" | "O" = "X";
+let winner: Cell | "Draw" = "";
 
 function createCell(row: number, col: number, content: Cell = "") {
   const cell = document.createElement("button");
@@ -28,14 +29,82 @@ function createCell(row: number, col: number, content: Cell = "") {
   cell.classList.add("cell");
 
   cell.addEventListener("click", () => {
+    if (winner) return;
     if (boardState[row][col] === "") {
       boardState[row][col] = currentMove;
       currentMove = currentMove === "X" ? "O" : "X";
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      winner = checkBoard();
       // eslint-disable-next-line @typescript-eslint/no-use-before-define
       renderBoard();
     }
   });
   return cell;
+}
+type Coordinate = [number, number];
+type Victory = [Coordinate, Coordinate, Coordinate];
+const victories: Victory[] = [
+  [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ],
+  [
+    [1, 0],
+    [1, 1],
+    [1, 2],
+  ],
+  [
+    [2, 0],
+    [2, 1],
+    [2, 2],
+  ],
+  [
+    [0, 0],
+    [1, 0],
+    [2, 0],
+  ],
+  [
+    [0, 1],
+    [1, 1],
+    [2, 1],
+  ],
+  [
+    [0, 2],
+    [1, 2],
+    [2, 2],
+  ],
+  [
+    [0, 0],
+    [1, 1],
+    [2, 2],
+  ],
+  [
+    [0, 2],
+    [1, 1],
+    [2, 0],
+  ],
+];
+function checkBoard(): Cell | "Draw" {
+  for (let victory of victories) {
+    const cell1 = boardState[victory[0][0]][victory[0][1]];
+    const cell2 = boardState[victory[1][0]][victory[1][1]];
+    const cell3 = boardState[victory[2][0]][victory[2][1]];
+    if (cell1 !== "" && cell1 === cell2 && cell2 === cell3) {
+      return cell1;
+    }
+  }
+
+  // Check stalemate first
+  let isDraw = true;
+  for (let i = 0; i < ROW_COUNT; i++) {
+    for (let j = 0; j < COL_COUNT; j++) {
+      if (boardState[i][j] === "") isDraw = false;
+    }
+  }
+  if (isDraw) return "Draw";
+
+  return "";
 }
 
 function renderBoard() {
@@ -53,7 +122,9 @@ function renderBoard() {
   }
   const moveElement = document.createElement("p");
   moveElement.id = "move-element";
-  moveElement.innerText = `Next Move: ${currentMove}`;
+  moveElement.innerText = winner
+    ? `Winner: ${winner}!`
+    : `Next Move: ${currentMove}`;
   moveElement.classList.add("current-move");
   appElement.insertBefore(moveElement, document.getElementById("reset"));
 }
@@ -68,6 +139,7 @@ function init() {
       ["", "", ""],
     ];
     currentMove = "X";
+    winner = "";
     renderBoard();
   });
   renderBoard();
