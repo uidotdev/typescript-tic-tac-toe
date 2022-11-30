dc03701f76595468d81cf4ddf32e3f905f4d672b
Karthik K <93112386+karthik-k01@users.noreply.github.com>
2022-11-30 11:59:10 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,111 @@ const boardElement = document.getElementById("board");
 const ROW_COUNT = 3;
 const COL_COUNT = 3;
 
-let boardState = [
+type Cell = "X" | "O" | "";
+
+type TicTacToeBoard = [
+  [Cell, Cell, Cell],
+  [Cell, Cell, Cell],
+  [Cell, Cell, Cell]
+];
+let boardState: TicTacToeBoard = [
   ["", "", ""],
   ["", "", ""],
   ["", "", ""]
 ];
-let currentMove = "X";
 
-function createCell(row, col, content = "") {
+let currentMove: "X" | "O" = "X";
+
+let winner: Cell | "Draw" = "";
+const victories: Victory[] = [
+  [
+    [0, 0],
+    [0, 1],
+    [0, 2]
+  ],
+  [
+    [1, 0],
+    [1, 1],
+    [1, 2]
+  ],
+  [
+    [2, 0],
+    [2, 1],
+    [2, 2]
+  ],
+  [
+    [0, 0],
+    [1, 0],
+    [2, 0]
+  ],
+  [
+    [0, 1],
+    [1, 1],
+    [2, 1]
+  ],
+  [
+    [0, 2],
+    [1, 2],
+    [2, 2]
+  ],
+  [
+    [0, 0],
+    [1, 1],
+    [2, 2]
+  ],
+  [
+    [0, 2],
+    [1, 1],
+    [2, 0]
+  ]
+];
+function checkBoard(): Cell | "Draw" {
+  for (let victory of victories) {
+    const cell1 = boardState[victory[0][0]][victory[0][1]];
+    const cell2 = boardState[victory[1][0]][victory[1][1]];
+    const cell3 = boardState[victory[2][0]][victory[2][1]];
+
+    if (cell1 !== "" && cell1 === cell2 && cell1 === cell3) {
+      return cell1;
+    }
+  }
+  let isDraw = true;
+
+  for (let i = 0; i < ROW_COUNT; i++) {
+    for (let j = 0; j < COL_COUNT; j++) {
+      if (boardState[i][j] === "") isDraw = false;
+    }
+  }
+
+  if (isDraw) return "Draw";
+
+  return "";
+}
+
+function createCell(row: number, col: number, content: Cell = "") {
   const cell = document.createElement("button");
   cell.setAttribute("data-row", row.toString());
   cell.setAttribute("data-col", col.toString());
   cell.setAttribute("data-content", content);
   cell.classList.add("cell");
+
+  cell.addEventListener("click", () => {
+    if (winner) return;
+    if (boardState[row][col] === "") {
+      boardState[row][col] = currentMove;
+      currentMove = currentMove === "X" ? "O" : "X";
+
+      winner = checkBoard();
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      renderBoard();
+    }
+  });
   return cell;
 }
+
+type Coordinates = [number, number];
+
+type Victory = [Coordinates, Coordinates, Coordinates];
 
 function renderBoard() {
   if (!appElement) throw new Error("Cannot find app");
@@ -36,7 +126,9 @@ function renderBoard() {
   }
   const moveElement = document.createElement("p");
   moveElement.id = "move-element";
-  moveElement.innerText = `Next Move: ${currentMove}`;
+  moveElement.innerText = winner
+    ? `Winner: ${winner}`
+    : `Next Move: ${currentMove}`;
   moveElement.classList.add("current-move");
   appElement.insertBefore(moveElement, document.getElementById("reset"));
 }
@@ -51,6 +143,7 @@ function init() {
       ["", "", ""]
     ];
     currentMove = "X";
+    winner = "";
     renderBoard();
   });
   renderBoard();
